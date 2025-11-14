686d103cae4674b36cfc306e2afe844dfafcbd11
JeanRoyen <jean.royen@student.hepl.be>
2025-11-14 16:51:30 +0100

    Create footer.blade.php

---
+++ resources/views/components/general/footer.blade.php
@@ -0,0 +1,39 @@
+<footer class="bg-white text-text-brown flex justify-evenly p-8">
+    <div>
+        <h2 class="font-bold text-2xl">Navigation</h2>
+        <ul class="underline">
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Nos animaux</a></li>
+            <li><a href="#">Adoption</a></li>
+            <li><a href="#">Devenir bénévole</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </div>
+    <div>
+        <h2 class="font-bold text-2xl">Contactez-nous</h2>
+        <ul>
+            <li><a href="tel:+32471420854" class="underline">0471 42 08 54</a></li>
+            <li><a href="mailto:jeanroyen2@hotmail.com" class="underline">jeanroyen2@hotmail.com</a></li>
+            <li><p>Avenue des Animaux,</p>
+                <p>140 Bruxelles, Belgique</p>
+            </li>
+        </ul>
+    </div>
+    <div>
+        <h2 class="font-bold text-2xl">Nos horaires</h2>
+        <div>
+            <p><span class="font-bold">Du lundi au samedi</span> de 10h à 18h</p>
+            <p>Fermé le dimanche et jours fériés</p>
+        </div>
+    </div>
+    <div>
+        <h2 class="font-bold text-2xl">Langues</h2>
+        <ul class="underline">
+            <li><a href="#">Français</a></li>
+            <li><a href="#">Anglais</a></li>
+        </ul>
+    </div>
+    <div>
+        <img src="{{ asset('images/LPH_logo.png') }}" alt="logo du refuge les pattes heureuses" width="96px">
+    </div>
+</footer>
