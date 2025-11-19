4ff1a0c11851956fbe03e9e18888032ee7acba5d
JeanRoyen <jean.royen@student.hepl.be>
2025-11-19 14:09:07 +0100

    Create card.blade.php

---
+++ resources/views/components/general/card.blade.php
@@ -0,0 +1,11 @@
+<div class="bg-white inline-block rounded-card overflow-hidden max-w-[300px]">
+    <div>
+        <img src="{{ asset('images/dog1.png') }}" alt="" class="block w-full"/>
+    </div>
+    <div class="p-3">
+        <span>3 ans</span>
+        <h3>Bob</h3>
+        <h4>Berger Malinois</h4>
+        <p>Bob est très affectueux avec les enfants, même en bas âge, il est également fort énergique.</p>
+    </div>
+</div>
