7ae1b406e449d6488dd192efe883e07504f3839b
JeanRoyen <jean.royen@student.hepl.be>
2025-11-19 16:31:28 +0100

    Update card.blade.php

--- resources/views/components/general/card.blade.php
+++ resources/views/components/general/card.blade.php
@@ -1,11 +1,12 @@
-<div class="bg-white inline-block rounded-card overflow-hidden max-w-[300px]">
+<div class="bg-white inline-block rounded-card overflow-hidden max-w-[300px] min-w-">
     <div>
         <img src="{{ asset('images/dog1.png') }}" alt="" class="block w-full"/>
     </div>
-    <div class="p-3">
-        <span>3 ans</span>
-        <h3>Bob</h3>
+    <div class="p-4">
+        <span class="text-background-green italic font-bold">3 ans</span>
+        <h3 class="font-bold md:text-4xl">Bob</h3>
         <h4>Berger Malinois</h4>
         <p>Bob est très affectueux avec les enfants, même en bas âge, il est également fort énergique.</p>
+        <a href="#" class="bg-cta-orange hover:bg-cta-hover text-white py-2 px-4 block text-center rounded-button">En savoir plus -></a>
     </div>
 </div>
