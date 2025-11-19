db427e628e12676524c136ce4cceaa5e1dfdb316
JeanRoyen <jean.royen@student.hepl.be>
2025-11-19 17:23:57 +0100

    Update div to article & update responsive

--- resources/views/components/form/contactForm.blade.php
+++ resources/views/components/form/contactForm.blade.php
@@ -1,9 +1,9 @@
-<form action="/" class="bg-white inline-block shadow-general rounded-card py-6 px-10 md:py-12 md:px-22 w-8/10">
-    <div class="md:flex gap-5">
-        <x-form.input name="name" title="Nom*" type="text" placeholder="Sarah "/>
+<form action="/" class="bg-white inline-block shadow-general rounded-card py-6 px-10 md:py-12 md:px-22 w-full">
+    <div class="md:flex gap-5 md:mb-4">
+        <x-form.input name="name" title="Nom*" type="text" placeholder="Sarah Gérard"/>
         <x-form.input name="phone" title="Numero de téléphone*" type="tel" placeholder="0471 42 08 46"/>
     </div>
-    <div>
+    <div class="md:mb-4">
         <x-form.input name="email" title="Adresse mail*" type="email" placeholder="Sarah@mail.be"/>
     </div>
     <div class="mb-8">
