e68234b7282af44423fad7b5406ca67b6ce97b72
JeanRoyen <jean.royen@student.hepl.be>
2025-11-19 16:29:16 +0100

    Create contactForm.blade.php

---
+++ resources/views/components/form/contactForm.blade.php
@@ -0,0 +1,15 @@
+<form action="/" class="bg-white inline-block shadow-general rounded-card py-6 px-10 md:py-12 md:px-22 w-8/10">
+    <div class="md:flex gap-5">
+        <x-form.input name="name" title="Nom*" type="text" placeholder="Sarah "/>
+        <x-form.input name="phone" title="Numero de téléphone*" type="tel" placeholder="0471 42 08 46"/>
+    </div>
+    <div>
+        <x-form.input name="email" title="Adresse mail*" type="email" placeholder="Sarah@mail.be"/>
+    </div>
+    <div class="mb-8">
+        <x-form.textarea name="message" placeholder="Écrire mon message..."/>
+    </div>
+    <div>
+        <x-form.button title="Envoyer mon message" />
+    </div>
+</form>
