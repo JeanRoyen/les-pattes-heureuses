f9336a10dd11d448e96adcd4bb61448971aecb7c
JeanRoyen <jean.royen@student.hepl.be>
2025-11-19 17:24:09 +0100

    Update responsive

--- resources/views/components/general/card.blade.php
+++ resources/views/components/general/card.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-white inline-block rounded-card overflow-hidden max-w-[300px] min-w-">
+<article class="bg-white inline-block rounded-card overflow-hidden max-w-[300px] mx-auto">
     <div>
         <img src="{{ asset('images/dog1.png') }}" alt="" class="block w-full"/>
     </div>
@@ -9,4 +9,4 @@
         <p>Bob est très affectueux avec les enfants, même en bas âge, il est également fort énergique.</p>
         <a href="#" class="bg-cta-orange hover:bg-cta-hover text-white py-2 px-4 block text-center rounded-button">En savoir plus -></a>
     </div>
-</div>
+</article>
