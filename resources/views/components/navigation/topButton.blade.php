e96ed093921586cdbd6b99e8b55bcce8e63bc06b
JeanRoyen <jean.royen@student.hepl.be>
2025-11-19 16:29:18 +0100

    Create topButton.blade.php

---
+++ resources/views/components/navigation/topButton.blade.php
@@ -0,0 +1,4 @@
+<a href="{{ $route }}"
+   class="inline-block text-sm px-5 py-3 leading-none border rounded text-white hover:bg-cta-hover bg-cta-orange border-cta-orange transition-colors mt-4 md:mt-0">
+    {{ $title }}
+</a>
