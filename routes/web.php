756e81b94c32f1413fab09840ae100b244702dd7
JeanRoyen <jean.royen@student.hepl.be>
2025-11-28 15:20:48 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+
+Route::get('/animals', function () {
+    return view('pages.animal-list');
+})->name('pages.animals-list');
