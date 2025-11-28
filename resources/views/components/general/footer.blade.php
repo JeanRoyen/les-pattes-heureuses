92284fb206168109c60c65fca8651101b27e566a
JeanRoyen <jean.royen@student.hepl.be>
2025-11-28 15:30:42 +0100

    Update footer to follow grid

--- resources/views/components/general/footer.blade.php
+++ resources/views/components/general/footer.blade.php
@@ -1,42 +1,44 @@
-<footer class="bg-white text-text-brown p-8 md:flex md:justify-between">
-    <div class="mb-4 md:mb-0">
-        <ul>
-            <nav>
-                <h2 class="font-bold">Navigation</h2>
-                <li class="hover:underline"><a href="#">Accueil</a></li>
-                <li class="hover:underline"><a href="#">Nos animaux</a></li>
-                <li class="hover:underline"><a href="#">Adoption</a></li>
-                <li class="hover:underline"><a href="#">Devenir bénévole</a></li>
-                <li class="hover:underline"><a href="#">Contact</a></li>
-            </nav>
-        </ul>
-    </div>
-    <div class="mb-4 md:mb-0">
-        <h2 class="font-bold">Contactez-nous</h2>
-        <ul>
-            <li><a href="tel:+32471420854" class="hover:underline">0471 42 08 54</a></li>
-            <li><a href="mailto:jeanroyen2@hotmail.com" class="hover:underline">jeanroyen2@hotmail.com</a></li>
-            <li><p>Avenue des Animaux,</p>
-                <p>140 Bruxelles, Belgique</p>
-            </li>
-        </ul>
-    </div>
-    <div class="mb-4 md:mb-0">
-        <h2 class="font-bold">Nos horaires</h2>
-        <div>
-            <p><span class="font-bold">Du lundi au samedi</span> de 10h à 18h</p>
-            <p>Fermé le dimanche et jours fériés</p>
+<footer class="bg-white text-text-brown">
+    <div class="container mx-auto px-4 md:px-0 md:flex md:justify-between py-6">
+        <div class="mb-4 md:mb-0">
+            <ul>
+                <nav>
+                    <h2 class="font-bold">Navigation</h2>
+                    <li class="hover:underline"><a href="#">Accueil</a></li>
+                    <li class="hover:underline"><a href="#">Nos animaux</a></li>
+                    <li class="hover:underline"><a href="#">Adoption</a></li>
+                    <li class="hover:underline"><a href="#">Devenir bénévole</a></li>
+                    <li class="hover:underline"><a href="#">Contact</a></li>
+                </nav>
+            </ul>
         </div>
-    </div>
-    <div class="mb-4 md:mb-0">
-        <h2 class="font-bold">Langues</h2>
-        <ul>
-            <li class="hover:underline"><a href="#">Français</a></li>
-            <li class="hover:underline"><a href="#">Anglais</a></li>
-        </ul>
-    </div>
-    <div>
-        <img src="{{ asset('images/LPH_logo.png') }}" alt="logo du refuge les pattes heureuses"
-             class="w-30 md:w-20">
+        <div class="mb-4 md:mb-0">
+            <h2 class="font-bold">Contactez-nous</h2>
+            <ul>
+                <li><a href="tel:+32471420854" class="hover:underline">0471 42 08 54</a></li>
+                <li><a href="mailto:jeanroyen2@hotmail.com" class="hover:underline">jeanroyen2@hotmail.com</a></li>
+                <li><p>Avenue des Animaux,</p>
+                    <p>140 Bruxelles, Belgique</p>
+                </li>
+            </ul>
+        </div>
+        <div class="mb-4 md:mb-0">
+            <h2 class="font-bold">Nos horaires</h2>
+            <div>
+                <p><span class="font-bold">Du lundi au samedi</span> de 10h à 18h</p>
+                <p>Fermé le dimanche et jours fériés</p>
+            </div>
+        </div>
+        <div class="mb-4 md:mb-0">
+            <h2 class="font-bold">Langues</h2>
+            <ul>
+                <li class="hover:underline"><a href="#">Français</a></li>
+                <li class="hover:underline"><a href="#">Anglais</a></li>
+            </ul>
+        </div>
+        <div>
+            <img src="{{ asset('images/LPH_logo.png') }}" alt="logo du refuge les pattes heureuses"
+                 class="w-30 md:w-20">
+        </div>
     </div>
 </footer>
