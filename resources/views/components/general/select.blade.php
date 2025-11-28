d4d1eba251864855a8dd10d11e9912b611154d4d
JeanRoyen <jean.royen@student.hepl.be>
2025-11-28 15:21:24 +0100

    Create select.blade.php

---
+++ resources/views/components/general/select.blade.php
@@ -0,0 +1,7 @@
+<div class="col-span-8 md:col-span-4 lg:col-span-2">
+    <label for="{{ $name }}" class="block mb-2.5">{{ $title }}</label>
+    <select id="{{ $name }}"
+            class="bg-white text-black w-full px-4 py-2 border-gray-400 border rounded-button">
+        {{ $slot }}
+    </select>
+</div>
