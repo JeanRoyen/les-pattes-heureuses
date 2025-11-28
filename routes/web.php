43e200b6ddd3e49af4927e7ebd788656ca807b36
JeanRoyen <jean.royen@student.hepl.be>
2025-11-28 20:32:06 +0100

    Changed dashboard -> login

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@ Route::get('/animals', function () {
     return view('pages.animal-list');
 })->name('pages.animals-list');
 
-Route::get('/dashboard', function () {
-    return view('pages.admin.dashboard');
-})->name('admin.dashboard');
+Route::get('/login', function () {
+    return view('pages.admin.login');
+})->name('admin.login');
