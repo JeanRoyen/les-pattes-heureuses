0dcfc40be9da37b3ec4c3e91f77785658945756f
JeanRoyen <jean.royen@student.hepl.be>
2025-11-29 16:27:48 +0100

    Update login.blade.php

--- resources/views/pages/admin/login.blade.php
+++ resources/views/pages/admin/login.blade.php
@@ -12,9 +12,9 @@
 <main>
     <div class="flex flex-col justify-center items-center m-10">
         <div>
-            <img src="{{ asset('images/LPH_logo.png') }}" alt="" width="300">
+            <img src="{{ asset('images/LPH_logo.png') }}" alt="" width="200">
         </div>
-        <div class="w-full">
+        <div class="md:w-1/2 w-full">
             <form action="/"
                   class="bg-white shadow-general rounded-card p-4 md:p-8">
                 <div class="mb-4">
@@ -26,12 +26,12 @@
                 <div>
                     <x-form.button title="Se connecter"/>
                 </div>
+                <div class="pt-4">
+                    <a href="#" class="underline hover:text-background-green">Mot de passe oublié</a>
+                </div>
             </form>
         </div>
     </div>
 </main>
-<footer>
-
-</footer>
 </body>
 </html>
