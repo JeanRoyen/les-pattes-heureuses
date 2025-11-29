c4290c5cb82f7308f5b95f973ff9b30e7d276901
JeanRoyen <jean.royen@student.hepl.be>
2025-11-29 16:27:42 +0100

    Update outline

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,4 +1,4 @@
 <div class="grow">
     <label for="{{ $name }}" class="block">{{ $title }}</label>
-    <input type="{{ $type }}" id="{{ $name }}" placeholder="{{ $placeholder }}" class="border-input-grey border-1 rounded-button pl-2 w-full py-1">
+    <input type="{{ $type }}" id="{{ $name }}" placeholder="{{ $placeholder }}" class="border-input-grey border-1 rounded-button pl-2 w-full py-1 focus:border-background-green focus:outline-none">
 </div>
