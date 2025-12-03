36a1b006ced926ef5d3841a5e85367f9c1d8ed78
JeanRoyen <jean.royen@student.hepl.be>
2025-12-03 14:50:00 +0100

    Update sideBar.blade.php

--- resources/views/components/admin/sideBar.blade.php
+++ resources/views/components/admin/sideBar.blade.php
@@ -1,7 +1,8 @@
 {{--TODO: Responsive sidebar--}}
-<aside class="w-64 bg-white shadow-lg fixed left-0 top-20 bottom-0 overflow-y-auto z-40">
+<aside class="w-64 bg-white shadow-md fixed left-0 top-20 bottom-0 overflow-y-auto z-40">
     <div class="p-6">
-        <h2 class="text-2xl font-bold text-text-brown mb-6">Bonjour Elise</h2>
+        <h2 class="sr-only">Navigation sur le coté</h2>
+        <h3 class="text-2xl font-bold text-text-brown mb-6">Bonjour Elise</h3>
         <ul>
             <x-admin.listElement title="Tableau de bord" route="#"/>
             <x-admin.listElement title="Animaux" route="#"/>
