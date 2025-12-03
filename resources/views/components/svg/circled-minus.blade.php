fa65f5e8cdaaac531cb5867b73e5a9286faed18a
JeanRoyen <jean.royen@student.hepl.be>
2025-12-03 14:50:38 +0100

    Create svg

---
+++ resources/views/components/svg/circled-minus.blade.php
@@ -0,0 +1,10 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" viewBox="0 0 46 46" fill="none">
+    <g clip-path="url(#clip0_61_2341)">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M31.625 21.5625H14.375C13.5801 21.5625 12.9375 22.2094 12.9375 23C12.9375 23.7906 13.5801 24.4375 14.375 24.4375H31.625C32.4199 24.4375 33.0625 23.7906 33.0625 23C33.0625 22.2094 32.4199 21.5625 31.625 21.5625ZM23 46C10.2968 46 0 35.7075 0 23C0 10.2925 10.2968 0 23 0C35.7032 0 46 10.2925 46 23C46 35.7075 35.7032 46 23 46Z" fill="white"/>
+    </g>
+    <defs>
+        <clipPath id="clip0_61_2341">
+            <rect width="46" height="46" fill="white" transform="matrix(1 0 0 -1 0 46)"/>
+        </clipPath>
+    </defs>
+</svg>
