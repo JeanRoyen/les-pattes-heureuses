92d4b7a2e74d449f43c75393b064cc51e7d39e6e
JeanRoyen <jean.royen@student.hepl.be>
2025-12-05 17:23:47 +0100

    Create Animal.php

---
+++ app/Models/Animal.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+
+class Animal extends Model
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = ['name', 'specie', 'race', 'age', 'gender', 'vaccine', 'description', 'status'];
+}
