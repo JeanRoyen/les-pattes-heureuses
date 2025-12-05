d1035bf15b6866d927c017eabdc5207e4e35ebc4
JeanRoyen <jean.royen@student.hepl.be>
2025-12-05 17:25:11 +0100

    Create AnimalFactory.php

---
+++ database/factories/AnimalFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Animal;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Animal>
+ */
+class AnimalFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Animal::class;
+
+    public function definition(): array
+    {
+
+        return [
+            'name' => $this->faker->firstName(),
+            'specie' => $this->faker->randomElement(['Chien', 'Chat', 'Furet']),
+            'race' => $this->faker->word(),
+            'age' => $this->faker->dateTimeBetween('-15 years', 'now'),
+            'gender' => $this->faker->boolean(),
+            'vaccine' => $this->faker->randomElement(['À jour', 'Non vacciné', 'Partiellement vacciné']),
+            'description' => $this->faker->sentence(8),
+            'status' => $this->faker->randomElement(['Disponible', 'Adopté', 'En soins']),
+            'avatar' => $this->faker->imageUrl(200, 200, 'animals', true),
+        ];
+    }
+}
