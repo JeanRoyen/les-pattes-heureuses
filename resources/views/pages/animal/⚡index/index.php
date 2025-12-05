1f019bbde998ddd1113a25855f200d21bc8539ce
JeanRoyen <jean.royen@student.hepl.be>
2025-12-05 17:24:39 +0100

    Update index.php

--- resources/views/pages/animal/⚡index/index.php
+++ resources/views/pages/animal/⚡index/index.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Models\Animal;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 new class extends Component
 {
-    //
+    #[Computed]
+    public function animals()
+    {
+        return Animal::all();
+    }
 };
