e24d115e4d653143714764abafed87c41ba08c8e
JeanRoyen <jean.royen@student.hepl.be>
2025-12-05 17:25:07 +0100

    Create 2025_12_05_134103_create_animals_table.php

---
+++ database/migrations/2025_12_05_134103_create_animals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->string('avatar')->nullable();
+            $table->string('name');
+            $table->string('specie');
+            $table->string('race');
+            $table->timestamp('age');
+            $table->boolean('gender');
+            $table->string('vaccine');
+            $table->text('description');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animals');
+    }
+};
