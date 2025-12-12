a67da3e954552f75dc5fdd0be615df2c9f7ff4ad
JeanRoyen <jean.royen@student.hepl.be>
2025-12-12 14:29:53 +0100

    Test + view for waiting Animals

--- resources/views/pages/animal/⚡index/index.blade.php
+++ resources/views/pages/animal/⚡index/index.blade.php
@@ -67,13 +67,30 @@
         <x-admin.table>
             <tr>
                 <x-admin.table-header title="Nom"/>
-                <x-admin.table-header title="Espèce"/>
+                <x-admin.table-header title="Race"/>
                 <x-admin.table-header title="Description"/>
                 <x-admin.table-header title="Sexe"/>
                 <x-admin.table-header title="Naissance"/>
                 <x-admin.table-header title="Vaccins"/>
+                <x-admin.table-header title="Status"/>
                 <x-admin.table-header title="Action"/>
             </tr>
+            @forelse($this->waitingAnimals as $animal)
+                <tr>
+                    <x-admin.table-data title="{{ $animal->name }}"/>
+                    <x-admin.table-data title="{{ $animal->race }}"/>
+                    <x-admin.table-data title="{{ $animal->description }}"/>
+                    <x-admin.table-data title="{{ $animal->gender ? 'Mâle' : 'Femelle' }}"/>
+                    <x-admin.table-data title="{{ $animal->age->format('d/m/Y') }}"/>
+                    <x-admin.table-data title="{{ $animal->vaccine ? 'À jour' : 'À faire' }}"/>
+                    <x-admin.table-data title="{{ $animal->status }}"/>
+                    <x-admin.table-data title="Supprimer / Voir"/>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="8" class="text-center py-4 bg-white border">Pas d'animaux trouvés</td>
+                </tr>
+            @endforelse
         </x-admin.table>
     </x-admin.section-spacing>
     <x-admin.section-spacing>
--- tests/Feature/animal/index.test.php
+++ tests/Feature/animal/index.test.php
@@ -57,3 +57,52 @@ it('assert animal age exist on page', function () {
     Livewire::test('pages::animal.index')
         ->assertSee('24/03/2025')->assertSee('24/03/2023');
 });
+
+// Tests for waiting animals
+it('assert waiting animal name exist on page', function () {
+    Animal::factory()->create(['name' => 'Bob', 'status' => 'waiting']);
+    Animal::factory()->create(['name' => 'Ferdinand', 'status' => 'waiting']);
+    Livewire::test('pages::animal.index')
+        ->assertSee('Bob')->assertSee('Ferdinand');
+});
+
+it('assert waiting animal specie exist on page', function () {
+    Animal::factory()->create(['race' => 'Berger Allemand', 'status' => 'waiting']);
+    Animal::factory()->create(['race' => 'Berger Australien', 'status' => 'waiting']);
+    Livewire::test('pages::animal.index')
+        ->assertSee('Berger Allemand')->assertSee('Berger Australien');
+});
+
+it('assert waiting animal description exist on page', function () {
+    Animal::factory()->create(['description' => 'Bob est un berger Allemand', 'status' => 'waiting']);
+    Animal::factory()->create(['description' => 'Ferdinand est un berger Australien', 'status' => 'waiting']);
+    Livewire::test('pages::animal.index')
+        ->assertSee('Bob est un berger Allemand')->assertSee('Ferdinand est un berger Australien');
+});
+
+it('assert waiting animal gender exist on page', function () {
+    Animal::factory()->create(['gender' => true, 'status' => 'waiting']);
+    Animal::factory()->create(['gender' => false, 'status' => 'waiting']);
+    Livewire::test('pages::animal.index')
+        ->assertSee('Mâle')->assertSee('Femelle');
+});
+
+it('assert waiting animal status exist on page', function () {
+    Animal::factory()->create(['status' => 'waiting']);
+    Livewire::test('pages::animal.index')
+        ->assertSee('waiting');
+});
+
+it('assert waiting animal vaccine exist on page', function () {
+    Animal::factory()->create(['vaccine' => true, 'status' => 'waiting']);
+    Animal::factory()->create(['vaccine' => false, 'status' => 'waiting']);
+    Livewire::test('pages::animal.index')
+        ->assertSee('À jour')->assertSee('À faire');
+});
+
+it('assert waiting animal age exist on page', function () {
+    Animal::factory()->create(['age' => '2025-03-24 00:00:00', 'status' => 'waiting']);
+    Animal::factory()->create(['age' => '2023-03-24 00:00:00', 'status' => 'waiting']);
+    Livewire::test('pages::animal.index')
+        ->assertSee('24/03/2025')->assertSee('24/03/2023');
+});
