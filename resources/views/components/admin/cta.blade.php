e5eb4d70e6c33b590ec894d41940adf5abc176b6
JeanRoyen <jean.royen@student.hepl.be>
2025-12-15 14:12:14 +0100

    Update cta.blade.php

--- resources/views/components/admin/cta.blade.php
+++ resources/views/components/admin/cta.blade.php
@@ -1,3 +1,5 @@
-<a href="#" class="p-3 bg-cta-orange text-white rounded-button">
-    {{ $title }}
-</a>
+<div x-data="{ open: false }">
+    <button x-on:click="open = ! open" class="p-3 bg-cta-orange text-white rounded-button">
+        Ajouter un animal
+    </button>
+</div>
