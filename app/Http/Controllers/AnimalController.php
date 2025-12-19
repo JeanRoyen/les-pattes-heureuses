625fb17913a84e45a9f3bf8949d276bfa469a529
JeanRoyen <jean.royen@student.hepl.be>
2025-12-19 16:11:55 +0100

    Update AnimalController.php

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -11,7 +11,6 @@ class AnimalController extends Controller
 {
     public function index(): View
     {
-
         return view('pages.animal-list.index', [
             'animals' => Animal::whereIn('status', ['in_care', 'available', 'waiting'])->get(),
         ]);
@@ -31,7 +30,7 @@ class AnimalController extends Controller
     public function getMonthlyStats()
     {
         $from = now()->startOfMonth();
-        $to   = now()->endOfMonth();
+        $to = now()->endOfMonth();
 
         return [
             'month' => now()->monthName,
