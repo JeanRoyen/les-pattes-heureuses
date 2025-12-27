d417bdfa0bde93be275f32d5a3eacc6437fe5efc
JeanRoyen <jean.royen@student.hepl.be>
2025-12-27 14:42:02 +0100

    Add time to search request

--- resources/views/components/general/searchbar.blade.php
+++ resources/views/components/general/searchbar.blade.php
@@ -6,7 +6,7 @@
 <div>
     <input
         type="text"
-        wire:model.live="{{ $model }}"
+        wire:model.live.debounce.500ms="{{ $model }}"
         placeholder="{{ $placeholder }}"
         class="bg-white text-black w-full px-4 py-2 border-gray-400 border rounded-button focus:border-background-green focus:outline-none"
     >
