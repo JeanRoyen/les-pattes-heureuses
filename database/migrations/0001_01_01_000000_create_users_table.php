193cb3546170adfc7e79c3ffa1996a1d270d46b0
JeanRoyen <jean.royen@student.hepl.be>
2025-12-28 16:08:07 +0100

    SoftDeletes for users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->boolean('role');
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
 
