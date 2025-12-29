9f8f9e4cffe86a20e9790ceb282dbea006de52c3
JeanRoyen <jean.royen@student.hepl.be>
2025-12-29 15:46:13 +0100

    Dynamic hello message

--- resources/views/components/admin/sideBar.blade.php
+++ resources/views/components/admin/sideBar.blade.php
@@ -2,7 +2,7 @@
 <nav class="w-64 bg-white shadow-md fixed left-0 top-20 bottom-0 overflow-y-auto z-40">
     <div class="p-6">
         <h2 class="sr-only">Navigation sur le coté</h2>
-        <h3 class="text-2xl font-bold text-text-brown mb-6">Bonjour Elise</h3>
+        <h3 class="text-2xl font-bold text-text-brown mb-6">Bonjour {{ ucfirst(auth()->user()->name) }}</h3>
         <ul>
             <x-admin.listElement title="Tableau de bord" route="{{ route('admin.dashboard') }}"/>
             <x-admin.listElement title="Animaux" route="{{ route('admin.animals') }}"/>
