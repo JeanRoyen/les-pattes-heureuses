14fc2125ca89a183176140c8982e0581c08c922c
JeanRoyen <jean.royen@student.hepl.be>
2026-01-02 15:56:33 +0100

    Fixed title validation

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\NewMessageMail;
 use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class MessageController extends Controller
 {
@@ -12,10 +14,12 @@ class MessageController extends Controller
         $validated = $request->validate([
             'name' => 'required|string|max:255',
             'phone' => 'required|string|max:20',
-            'title' => 'required|string|max:20',
+            'title' => 'required|string|max:50',
             'email' => 'required|email|max:255',
             'message' => 'required|string',
         ]);
+
+        Mail::to('Elise@refuge.be')->send(new NewMessageMail());
 
         Message::create($validated);
         return back()->with('success', 'Message envoyé !');
