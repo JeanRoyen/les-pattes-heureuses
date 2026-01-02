eb7c30f8d01a4d610be2c9aa0a18882924be4c73
JeanRoyen <jean.royen@student.hepl.be>
2026-01-02 15:57:32 +0100

    Add mail to messages & view

---
+++ resources/views/view/email/messageView.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Vous avez reçu un nouveau message depuis le formulaire</h1>
+<p>Connectez vous pour consulter le message.</p>
+<a href="{{ route('admin.messages') }}">Voir le message</a>
+</body>
+</html>
