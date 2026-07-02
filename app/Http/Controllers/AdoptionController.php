4f346dede63b4850b91b9067624500f7af59d868
JeanRoyen <jean.royen@student.hepl.be>
2026-07-02 17:29:48 +0200

    Update | Phone number reformat before entering DB

--- app/Http/Controllers/AdoptionController.php
+++ app/Http/Controllers/AdoptionController.php
@@ -6,15 +6,18 @@ use App\Models\Adoption;
 use Illuminate\Http\Request;
 use function redirect;
 use function route;
+use function str_replace;
 
 class AdoptionController extends Controller
 {
     public function store(Request $request)
     {
+        $request['phone'] = str_replace(' ', '', $request['phone']);
+        
         $validated = $request->validate([
             'animal_id' => ['required', 'exists:animals,id'],
             'name' => ['required', 'string'],
-            'phone' => ['required', 'digits:10'],
+            'phone' => ['required', 'numeric'],
             'email' => ['required', 'email'],
             'message' => ['required', 'string'],
         ]);
